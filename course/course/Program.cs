a37e6dcbb0954a79b07cb520511722937f59a253
Sanvi Gowda <sanvi16sathish@gmail.com>
2025-10-24 10:33:52 +0530

    Add files via upload

---
+++ course/course/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+   class Course
+{
+    protected  void ShowDetails()
+    {
+        Console.WriteLine("course name:BCA");
+        Console.WriteLine("course duration:3 years");
+        Console.WriteLine("course fees:Rs. 148000");
+    }
+}
+class Program : Course
+{
+    static void Main()
+    {
+        Program p = new Program();
+        p.ShowDetails();
+        Console.WriteLine("course mode:Online");
+    }
+
+}
